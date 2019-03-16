7d83157b798a91319592d111c8be020df883091f
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2019-03-16 11:15:50 +0700

    Fix crashing in lbf and slow in kazemi w big faces

--- src/face_landmark_detector/face_landmark_detector_lbf.cpp
+++ src/face_landmark_detector/face_landmark_detector_lbf.cpp
@@ -24,8 +24,18 @@ std::vector<LandMarkResult> FaceLandmarkDetectorLBF::detect(const cv::Mat & img,
 
     // Face face bounding boxes
     std::vector<cv::Rect> face_rects;
+    std::vector<int> considered_faces_idx; // Indices of faces we put into face_rects to find landmarks
     for (size_t i = 0; i < faces.size(); ++i) {
-        face_rects.push_back(faces[i].getFaceRect());
+        
+        cv::Rect face_rect = faces[i].getFaceRect();
+
+        // Put face_rect into face_rects to find landmarks only if face_rect lies in the boundary of img.
+        // This prevents crashing because of facemark->fit(img, face_rects, shapes);
+        if ( 0 <= face_rect.x && 0 <= face_rect.width && face_rect.x + face_rect.width <= img.cols && 0 <= face_rect.y && 0 <= face_rect.height && face_rect.y + face_rect.height <= img.rows) {
+            face_rects.push_back(face_rect);
+            considered_faces_idx.push_back(i);
+        }
+
     }
 
     // Detect face landmarks
@@ -34,8 +44,8 @@ std::vector<LandMarkResult> FaceLandmarkDetectorLBF::detect(const cv::Mat & img,
 
 
     // Merge detected landmarks to landmark results;
-    for (size_t i = 0; i < faces.size(); ++i) {
-        faces[i].setFaceLandmark(shapes[i]);
+    for (size_t i = 0; i < considered_faces_idx.size(); ++i) {
+        faces[considered_faces_idx[i]].setFaceLandmark(shapes[i]);
     }
 
     return faces;
